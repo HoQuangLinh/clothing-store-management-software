e5ec2e732771773b462909f323dab5ffd09b7664
UIT.19521334 <67258104+UIT19521334@users.noreply.github.com>
2021-11-09 06:50:26 +0700

    update boosstrap

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import Navbar from "../navbar/Navbar";
+import NavBar from "../navbar/Navbar";
 import Sidebar from "../sidebar/Sidebar";
 import "./layout.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesLayout from "../../navigation/RoutesLayout";
-
 const Layout = () => {
   return (
     <Router>
       <Sidebar />
       <div className="layout__content">
-        <Navbar />
+        <NavBar />
         <div className="layout__content-routes">
           <RoutesLayout />
         </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,35 +1,50 @@
 import React from "react";
 import "./navbar.css";
 import cr7 from "../../assets/images/cr7.jpg";
-const Navbar = () => {
+const NavBar = () => {
+  function Dropdown(){
+    var x = document.getElementById("myDropdown");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  };
   return (
-    <div className="navbar">
-      <div className="navbar__left">
-        <i class="bx bx-menu"></i>
-        <p>Dashboard</p>
+    <div>
+      <div className="navbar">
+        <div className="navbar__left">
+          <i class="bx bx-menu"></i>
+          <p>Dashboard</p>
+        </div>
+
+        {
+          //<div className="navbar__search">
+          // <input type="text" placeholder="Tìm kiếm..." />
+          // <i className="bx bx-search"></i>
+          //</div>
+        }
+
+        <div className="navbar__right">
+          <div className="navbar__right-item">
+            <i className="bx bx-bell"></i>
+          </div>
+          <div className="navbar__right-item">
+            <i className="bx bx-palette" onClick="Dropdown()" ></i>
+            <buton onclick="Dropdown()"> bấm đây</buton>
+          </div>
+          <div className="navbar__right-item">
+            <img src={cr7} alt="" />
+            <span>RonalDo</span>
+          </div>
+        </div>
       </div>
-
-      {
-        //<div className="navbar__search">
-        // <input type="text" placeholder="Tìm kiếm..." />
-        // <i className="bx bx-search"></i>
-        //</div>
-      }
-
-      <div className="navbar__right">
-        <div className="navbar__right-item">
-          <i className="bx bx-bell"></i>
-        </div>
-        <div className="navbar__right-item">
-          <i className="bx bx-palette"></i>
-        </div>
-        <div className="navbar__right-item">
-          <img src={cr7} alt="" />
-          <span>RonalDo</span>
-        </div>
+      
+      <div id ="myDropdown" className="navnar__paleteteShow">
+        <div className="navbar__PlaColor_blue"></div> 
       </div>
     </div>
   );
 };
 
-export default Navbar;
+export default NavBar;
