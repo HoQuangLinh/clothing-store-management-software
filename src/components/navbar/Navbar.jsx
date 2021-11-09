61f04cde776e99f77244b3ee271e498172d4268e
UIT.19521334 <67258104+UIT19521334@users.noreply.github.com>
2021-11-09 09:30:27 +0700

    updateNavBar
    color picker

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import "./navbar.css";
 import cr7 from "../../assets/images/cr7.jpg";
 const NavBar = () => {
-  function Dropdown(){
-    var x = document.getElementById("myDropdown");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  /* When the user clicks on the button, 
+  toggle between hiding and showing the dropdown content */
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
   };
   return (
     <div>
@@ -29,19 +26,26 @@ const NavBar = () => {
           <div className="navbar__right-item">
             <i className="bx bx-bell"></i>
           </div>
-          <div className="navbar__right-item">
-            <i className="bx bx-palette" onClick="Dropdown()" ></i>
-            <buton onclick="Dropdown()"> bấm đây</buton>
+          <div className="navbar__right-icon">
+            <i className="bx bx-palette" onClick={myFunction} ></i>
+            <div id ="myDropdown" className="navnar__paleteteShow"> 
+              <div className = "navbar__PlaColor_row">
+                <div className = "navbar__PlaColor_blue"> </div>     
+                <div className = "navbar__PlaColor_red"> </div> 
+                <div className = "navbar__PlaColor_pink"> </div> 
+              </div>    
+              <div className = "navbar__PlaColor_row">
+                <div className = "navbar__PlaColor_yellow"> </div>     
+                <div className = "navbar__PlaColor_green"> </div> 
+                <div className = "navbar__PlaColor_orange"> </div> 
+              </div>   
+            </div>
           </div>
           <div className="navbar__right-item">
             <img src={cr7} alt="" />
             <span>RonalDo</span>
           </div>
         </div>
-      </div>
-      
-      <div id ="myDropdown" className="navnar__paleteteShow">
-        <div className="navbar__PlaColor_blue"></div> 
       </div>
     </div>
   );
