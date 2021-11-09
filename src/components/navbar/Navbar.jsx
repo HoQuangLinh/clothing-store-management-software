7ffc7c13f0acc07c41c2b4c2e8fc84a96dd3b14d
Ho Quang Linh <hoquanglinh2710@gmail.com>
2021-11-09 13:01:39 +0700

    fix conflig

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,21 +2,6 @@ import React from "react";
 import "./navbar.css";
 import cr7 from "../../assets/images/cr7.jpg";
 const NavBar = () => {
-<<<<<<< HEAD
-  function Dropdown(){
-    var x = document.getElementById("myDropdown");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-=======
-  /* When the user clicks on the button, 
-  toggle between hiding and showing the dropdown content */
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
->>>>>>> 033c490dff1858959e3c052388e8a626fd493daf
-  };
   return (
     <div>
       <div className="navbar">
@@ -36,26 +21,20 @@ const NavBar = () => {
           <div className="navbar__right-item">
             <i className="bx bx-bell"></i>
           </div>
-<<<<<<< HEAD
-          <div className="navbar__right-item">
-            <i className="bx bx-palette" onClick="Dropdown()" ></i>
-            <buton onclick="Dropdown()"> bấm đây</buton>
-=======
           <div className="navbar__right-icon">
-            <i className="bx bx-palette" onClick={myFunction} ></i>
-            <div id ="myDropdown" className="navnar__paleteteShow"> 
-              <div className = "navbar__PlaColor_row">
-                <div className = "navbar__PlaColor_blue"> </div>     
-                <div className = "navbar__PlaColor_red"> </div> 
-                <div className = "navbar__PlaColor_pink"> </div> 
-              </div>    
-              <div className = "navbar__PlaColor_row">
-                <div className = "navbar__PlaColor_yellow"> </div>     
-                <div className = "navbar__PlaColor_green"> </div> 
-                <div className = "navbar__PlaColor_orange"> </div> 
-              </div>   
+            <i className="bx bx-palette"></i>
+            <div id="myDropdown" className="navnar__paleteteShow">
+              <div className="navbar__PlaColor_row">
+                <div className="navbar__PlaColor_blue"> </div>
+                <div className="navbar__PlaColor_red"> </div>
+                <div className="navbar__PlaColor_pink"> </div>
+              </div>
+              <div className="navbar__PlaColor_row">
+                <div className="navbar__PlaColor_yellow"> </div>
+                <div className="navbar__PlaColor_green"> </div>
+                <div className="navbar__PlaColor_orange"> </div>
+              </div>
             </div>
->>>>>>> 033c490dff1858959e3c052388e8a626fd493daf
           </div>
           <div className="navbar__right-item">
             <img src={cr7} alt="" />
@@ -63,9 +42,9 @@ const NavBar = () => {
           </div>
         </div>
       </div>
-      
-      <div id ="myDropdown" className="navnar__paleteteShow">
-        <div className="navbar__PlaColor_blue"></div> 
+
+      <div id="myDropdown" className="navnar__paleteteShow">
+        <div className="navbar__PlaColor_blue"></div>
       </div>
     </div>
   );
