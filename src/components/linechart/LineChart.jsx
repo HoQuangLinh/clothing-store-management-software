9d59ee3e9d2090a64a677a44234e1b690ed7ce21
Ho Quang Linh <hoquanglinh2710@gmail.com>
2021-12-10 20:35:17 +0700

    add dashboard api func

--- src/components/linechart/LineChart.jsx
+++ src/components/linechart/LineChart.jsx
@@ -43,34 +43,6 @@ export const options = {
   },
 };
 
-const labels = [
-  "Thứ hai",
-  "Thứ ba",
-  "Thứ tư",
-  "Thứ năm",
-  "Thứ sáu",
-  "Thứ bảy",
-  "Chủ nhật",
-];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Tuần trước",
-      data: [400, 120, 600, 300, 100, 200, 300],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Tuần này",
-      data: [600, 620, 300, 400, 300, 1000, 300],
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
-
-export function LineChart() {
+export function LineChart({ data }) {
   return <Line options={options} data={data} />;
 }
